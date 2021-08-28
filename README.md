660f03a888f359821cf71dbfdf7c3261891010d3
can-dy-jack <jjack_chen3@163.com>
2021-08-28 22:09:29 +0800

    fix all of this

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # web
 前端学习仓库
-
-> [源码仓库（vuepress）](https://gitee.com/kartjim/css) -->  [部署仓库]([can-dy-jack/web at vp-1.0.02 (github.com)](https://github.com/can-dy-jack/web/tree/vp-1.0.02)) -->  [知识库网址](https://kartjim.top/web/fisher/)
-
---
+++ assets/js/pub.js
@@ -0,0 +1,26 @@
+const box = document.querySelector('.box');
+const nav = document.querySelector('nav');
+
+// mode change 
+const ModeBtn = document.querySelector('.change-mode');
+const ModeList = document.querySelector('.mode-list');
+
+ModeBtn.onclick = function(){
+    ModeList.style.display = 'block';
+}
+function closeList(){
+    ModeList.style.display = 'none';
+}
+box.addEventListener('click',closeList);
+nav.addEventListener('click',closeList);
+
+
+// .code
+// add circle type
+const code = document.querySelectorAll('.code');
+for(let k=0;k<code.length;k++){
+    var circle = document.createElement('span');
+    circle.classList.add('circle');
+    code[k].appendChild(circle);
+}
+
